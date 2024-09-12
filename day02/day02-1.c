8098f21e668d7ae4a5b986a7d47ffcd898d7f30c
Hong Junho <nolja30@naver.com>
2024-09-12 13:35:42 +0900

    조건문 실습

---
+++ day02/day02-1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(void) {
+
+	int num1;
+	double num2, num3, num4 = 0;
+
+	printf("원하는 기능을 입력하시오.\n");
+	printf("1.더하기 2.빼기 3.곱하기 4.나누기\n");
+	printf("기능 : ");
+	scanf_s("%d",&num1);
+		
+	printf("숫자 1 입력 : ");
+	scanf_s("%lf", &num2);
+	printf("숫자 2 입력 : ");
+	scanf_s("%lf", &num3);
+
+	if (num1 == 1) {
+		num4 = num2 + num3;
+	}
+	else if (num1 == 2) {
+		num4 = num2 - num3;
+	}
+	else if (num1 == 3) {
+		num4 = num2 * num3;
+	}
+	else if (num1 == 4) {
+		num4 = num2 / num3;
+	}
+	else {
+		printf("잘못 입력하셨습니다.");
+
+	}
+
+	printf("%lf \n", num4);
+	return 0;
+}
