f4af9420c1b1c694b8e0f581e3d40374974f93a7
Hong Junho <nolja30@naver.com>
2024-10-10 13:27:45 +0900

    *피라미드 반복문실습

---
+++ day07/day07-1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(void) {
+
+	int num;
+	printf("피라미드의 층 수를 입력해 주세요 : ");
+	scanf_s("%d", &num);//층수 입력받기
+
+
+
+	for (int i = 1; i <= num; i++)//층 수만큰 반복
+	{
+		for (int o = 0; o < num - i; o++)//앞쪽 공백 입력 층수 - 반복수
+		{
+			printf(" ");
+			
+		}
+		for (int z = 0; z < i*2-1; z++)//*갯수 반복 : 반복수 * 2 - 1
+		{
+			printf("*");
+
+		}
+		printf("\n");//층 나누기
+
+	}
+
+
+
+	return 0;
+}
