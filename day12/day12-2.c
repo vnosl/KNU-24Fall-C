d2b13078f8f80facd0e3c8e31892e268d57db3ef
Hong Junho <nolja30@naver.com>
2024-12-01 21:13:24 +0900

    코딩테스트

---
+++ day12/day12-2.c
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int solution(int n)
+{
+    int ans = 0;
+
+    while (n > 0) {
+        if (n % 2 == 0) {
+            n /= 2;
+        }
+        else {
+            n--;
+            ans++;
+        }
+    }
+
+
+    return ans;
+}
